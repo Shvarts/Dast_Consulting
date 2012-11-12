9bb68efb3939e3ddc98d67a85c923594958d86d3
Shvarts <svarts@ukr.net>
2012-11-12 21:52:32 +0200

    delete search from index_controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,17 +5,17 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
-    @locs = Location.all
-        @search = params[:search]
-        @locations = []
-    @locs.each do |l|
-      if l.address.index(@search.to_s) && @search !="" && @search !=nil
-        @locations<<l
-      end
-    end
-    if @locations.empty?
-      @locations = Location.all
-    end
+    @locations = Location.all
+#        @search = params[:search]
+#        @locations = []
+#    @locs.each do |l|
+#      if l.address.index(@search.to_s) && @search !="" && @search !=nil
+#        @locations<<l
+#      end
+#    end
+#    if @locations.empty?
+#      @locations = Location.all
+#    end
     @json = @locations.to_gmaps4rails
     respond_with @json
   end
